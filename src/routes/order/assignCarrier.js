d5d407e4ad0429447c6a5a820e15072d15cfdad3
oneLovePC <onelovepc2021@gmail.com>
2025-05-02 14:34:05 +0530

    carrier assignments

--- src/routes/order/assignCarrier.js
+++ src/routes/order/assignCarrier.js
@@ -373,6 +373,35 @@ router.get('/carrier-assignments', jwtAuth.verifyToken, async (req, res) => {
 });
 
 
+router.get('/assigned-order-by-id', jwtAuth.verifyToken, async (req, res) => {
+    try {
+      const { order_ID } = req.query;
+  
+      if (!order_ID) {
+        return res.status(400).json({ message: 'order_ID is required in query.' });
+      }
+  
+      const [results] = await db.query(`
+        SELECT * FROM carrier_assignments WHERE order_ID = ?
+      `, [order_ID]);
+  
+      if (!results.length) {
+        return res.status(404).json({ message: 'No data found for the given order_ID.' });
+      }
+  
+      res.status(200).json({
+        message: 'Data fetched successfully.',
+        data: results
+      });
+  
+    } catch (error) {
+      logger.error('Error fetching assigning_orders:', error);
+      res.status(500).json({ message: 'Internal Server Error', error: error.message });
+    }
+  });
+  
+
+
 router.post('/carrier-assignment/confirm', jwtAuth.verifyToken, async (req, res) => {
     try {
         const {

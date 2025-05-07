c735bc1bacb284213c4aa2bcb1a253a9f76aff2a
oneLovePC <onelovepc2021@gmail.com>
2025-05-07 16:57:00 +0530

    test

--- src/routes/order/order.js
+++ src/routes/order/order.js
@@ -683,6 +683,7 @@ router.post('/create-order', jwtAuth.verifyToken, async (req, res) => {
         });
       }
     }
+    
 
     const resolvedProducts = packagesData.flatMap(p => p.products);
     const productIDs = resolvedProducts.map(rp => rp.prod_ID);

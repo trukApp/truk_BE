b93230e9d3411adbba53eb384a3fbbd6f5c3a5cb
komarajuKrishna <vamsi.komaraju@ro-one.in>
2025-05-21 13:35:52 +0530

    redelpoyment

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 
 const http = require('http');
-const app = require('./app'); 
-const {logger} = require('./src/logger/logger');
-const port = process.env.PORT || 8088;
+const app = require('./app');
+const { logger } = require('./src/logger/logger');
+const port = process.env.PORT || 80 || 8088;
 
-const server = http.createServer(app); 
+const server = http.createServer(app);
 
 server.listen(port, () => {
-    // console.log('Node server running on port ' + port);
-    logger.info('Node server running on port ' + port)
-  });
+  // console.log('Node server running on port ' + port);
+  logger.info('Node server running on port ' + port)
+});

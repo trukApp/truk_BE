d76a451da33486d9d4514a61695540d9a8ff71eb
komarajuKrishna <vamsi.komaraju@ro-one.in>
2025-05-21 13:40:14 +0530

    redelpoyment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const http = require('http');
 const app = require('./app');
 const { logger } = require('./src/logger/logger');
-const port = process.env.PORT || 80 || 8088;
+const port = process.env.PORT || 8088;
 
 const server = http.createServer(app);
 

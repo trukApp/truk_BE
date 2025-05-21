72c5d7dc8201967c4c8ebd5ac6da3402116c8810
oneLovePC <onelovepc2021@gmail.com>
2025-05-21 11:49:06 +0530

    domain check

--- src/routes/order/order.js
+++ src/routes/order/order.js
@@ -630,7 +630,6 @@ async function allocatePackages(packagesData, vehicles, sourceLocation, productM
   };
 }
 
-
 async function getPackagesByIds(packageIDs) {
   const placeholders = packageIDs.map(()=>'?').join(',');
   const [rows] = await db.query(`

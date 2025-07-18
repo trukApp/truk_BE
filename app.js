8e505deea9ebbbdac9d0e29d71f55423c60db9b5
KomarajuKrishna <vamsi.komaraju@ro-one.in>
2025-07-18 16:34:10 +0530

    redeploy

--- app.js
+++ app.js
@@ -86,6 +86,7 @@ const combinedSwaggerDocument = {
 app.use('/trukapp-api-docs', swaggerUi.serve, swaggerUi.setup(combinedSwaggerDocument));
 
 
+
 const signup = require('./src/routes/signup/signup');
 const login = require('./src/routes/login/login');
 const userData = require('./src/routes/user/userData');

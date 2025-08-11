967180bb03e3dc8ba02702f2ba5afeff77a3fc96
oneLovePC <onelovepc2021@gmail.com>
2025-08-11 12:27:22 +0530

    server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const http = require('http');
 const app = require('./app');
 const { logger } = require('./src/logger/logger');
-const port = process.env.PORT || 8088;
+const port = 8088;
 
 const server = http.createServer(app);
 

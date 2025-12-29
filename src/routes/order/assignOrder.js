23dfc6c5de05f044da7afd8ae8bc563a5d4081d1
KomarajuKrishna <vamsi.komaraju@ro-one.in>
2025-12-29 12:46:12 +0530

    modofied /assigned_order_status

--- src/routes/order/assignOrder.js
+++ src/routes/order/assignOrder.js
@@ -418,7 +418,7 @@ router.get('/assigned-order', jwtAuth.verifyToken, async (req, res) => {
             SELECT 
                 ao.assigning_id, ao.assign_ID, ao.order_ID, ao.assigned_vehicle_data, ao.self_transport, ao.pod, ao.pod_doc,ao.assigned_order_status,
                 o.scenario_label, o.total_cost, o.allocations, o.allocated_packages,
-                o.unallocated_packages, o.allocated_vehicles, o.created_at, o.updated_at, o.order_status, o.order_docs,
+                o.unallocated_packages, o.allocated_vehicles, o.created_at, o.updated_at, o.order_status, o.order_docs
             FROM assigning_orders ao
             LEFT JOIN orders o ON ao.order_ID = o.order_ID
             WHERE ${condition}
